e4b498ef8ba2fe4b511ea7c13d5477f384e3706e
Nick Salt <nicksalt24@gmail.com>
2021-10-26 20:31:51 -0400

    final changes

--- ShakepayApp/Services/ShakepayApiService.cs
+++ ShakepayApp/Services/ShakepayApiService.cs
@@ -12,25 +12,99 @@ namespace ShakepayApp.Services
         {
             _httpClient = httpClient;   
         }
+        public async Task<List<NetWorth>> GetNetWorthOverTime()
+        {
+            List<NetWorth> netWorths = new();
+            //Get API info
+            List<Transaction> transactions = (await GetTransactions()).OrderBy(t => t.createdAt).ToList();
+            var btcRates = await GetBTCRates();
+            var ethRates = await GetETHRates();
 
-        public async Task<List<Transaction>> GetTransactions()
+            //Set currency amount
+            decimal cad = 0m;
+            decimal btc = 0m;
+            decimal eth = 0m;
+
+            foreach (var transaction in transactions)
+            {
+                if (transaction.direction is null)
+                {
+                    if (transaction.from?.currency == "BTC")
+                    {
+                        btc -= transaction.from?.amount ?? 0m;
+                    }
+                    else if (transaction.from?.currency == "ETH")
+                    {
+                        eth -= transaction.from?.amount ?? 0m;
+                    }
+                    else
+                    {
+                        cad -= transaction.from?.amount ?? 0m;
+                    }
+
+                    if (transaction.to?.currency == "BTC")
+                    {
+                        btc += transaction.to?.amount ?? 0m;
+                    }
+                    else if (transaction.to?.currency == "ETH")
+                    {
+                        eth += transaction.to?.amount ?? 0m;
+                    }
+                    else
+                    {
+                        cad += transaction.to?.amount ?? 0m;
+                    }
+                }
+                else
+                {
+                    if (transaction.currency == "BTC")
+                        btc += transaction.amount ?? 0m * (transaction.direction == "credit" ? 1 : -1);
+                    else if (transaction.currency == "ETH")
+                        eth += transaction.amount ?? 0m * (transaction.direction == "credit" ? 1 : -1);
+                    else
+                        cad += transaction.amount ?? 0m * (transaction.direction == "credit" ? 1 : -1);
+                }
+                var currentNetWorth = GetCADValue(transaction.createdAt, btc, btcRates) + GetCADValue(transaction.createdAt, eth, ethRates) + (cad);
+                netWorths.Add(new NetWorth
+                {
+                    Date = transaction.createdAt,
+                    Worth = currentNetWorth
+                });
+            }
+            return netWorths;
+        }
+        private async Task<List<Transaction>> GetTransactions()
         {
             var result = await _httpClient.GetStringAsync("https://shakepay.github.io/programming-exercise/web/transaction_history.json"); 
             var listOfTransactions = JsonConvert.DeserializeObject<List<Transaction>>(result);
             return listOfTransactions;
         }
-        public async Task<List<Rate>> GetBTCRates()
+        private async Task<List<Rate>> GetBTCRates()
         {
             var result = await _httpClient.GetStringAsync("https://shakepay.github.io/programming-exercise/web/rates_CAD_BTC.json");
             var listOfRates = JsonConvert.DeserializeObject<List<Rate>>(result);
             return listOfRates;
         }
-        public async Task<List<Rate>> GetETHRates()
+        private async Task<List<Rate>> GetETHRates()
         {
             var result = await _httpClient.GetStringAsync("https://shakepay.github.io/programming-exercise/web/rates_CAD_ETH.json");
             var listOfRates = JsonConvert.DeserializeObject<List<Rate>>(result);
             return listOfRates;
         }
+            public decimal GetCADValue(DateTime currentDate, Decimal numberOfTokens, List<Rate> rates)
+            {
+                if (numberOfTokens == 0)
+                    return 0m;
+                var currentRate = rates.FirstOrDefault(r => r.createdAt.Date.Equals(currentDate.Date)) ?? rates.First();
+                return numberOfTokens * currentRate?.midMarketRate ?? 0m;
+
+            }
+        }
+
+    public record NetWorth
+    {
+        public DateTime Date { get; set; }  
+        public decimal Worth { get; set; }
     }
 
     public record Rate
---
+++ ShakepayApp/wwwroot/js/chart.js
@@ -0,0 +1,30 @@
+﻿window.chart = {
+    labels: [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+    ],
+    data: {
+        labels: this.labels,
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+    config: {
+        type: 'line',
+        data: this.data,
+        options: {}
+    },
+    create: function () {
+        const myChart = new Chart(
+            document.getElementById('myChart'),
+            config
+        );
+    }
+}
